5c492a223fbd2c4ca10ca84bd9ba3cf16388fcbc
Micha Schwendener <micha.schwendener@gmail.com>
2024-02-10 10:39:26 +0100

    feat: make tw + shadcn + font work

--- imports/ui/pages/MainPage.tsx
+++ imports/ui/pages/MainPage.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Card } from "../components/Card";
 
 export const MainPage = () => {
   return (
     <div>
-      <h1 className="text-red-600">Welcome to MainPage!</h1>
+      <Card>
+        <h1 className="text-red-600">Welcome to MainPage!</h1>
+      </Card>
     </div>
   );
 };
